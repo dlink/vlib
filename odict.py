899545cdc3bbb396a3dccdb85bdbf71dc85acf67
David Link <dvlink@gmail.com>
2013-01-31 15:56:09 -0500

    odict: added delete functionality

--- odict.py
+++ odict.py
@@ -12,7 +12,8 @@ class odict(dict):
 
     def __setattr__(self, key, value): self[key] = value
     def __getattr__(self, key): return self[key]
-    
+    def __delattr__(self, key): del self[key]    
+
 if __name__ == '__main__':
     #o = odict({'a':1, 2:3})
     o = odict(a=1, b=2)
