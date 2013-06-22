b41fb359e5acd406493af42217a7b804314f4b57
David Link <dlink@flatworldknowledge.com>
2013-06-22 09:52:11 -0400

    Version 0.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     url='https://github.com/dlink/vlib',
     packages=['vlib'],
     name='vlib',
-    version='0.1',
+    version='0.2',
     scripts=[],
     install_requires=map(string.strip, open('requirements.txt').readlines()),
 )
