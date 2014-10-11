00f4183106d7e11f8f34fc994a8a04c323bc1e49
David Link <dlink@gmail.com>
2014-10-11 17:30:48 +0000

    Getting pip install vlib to work properly

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     url='https://github.com/dlink/vlib',
     packages=['vlib'],
     name='vlib',
-    version='0.4',
+    version='0.7',
     scripts=[],
     license='GNU General Public License (GPL)',
     install_requires=map(string.strip, open('requirements.txt').readlines()),
