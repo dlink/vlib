caef4743a25b7ddc2ac529bb41b227ed075b2a26
David Link <dlink@napower.com>
2014-12-18 09:57:32 -0500

    setup version bump

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     url='https://github.com/dlink/vlib',
     packages=['vlib'],
     name='vlib',
-    version='0.9',
+    version='0.10',
     scripts=[],
     license='GNU General Public License (GPL)',
     install_requires=map(string.strip, open('requirements.txt').readlines()),
