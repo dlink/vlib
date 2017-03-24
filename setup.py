75625cddc5bbb8956ac9802525988544003590b7
David Link <david.link@tommiecopper.com>
2017-03-24 09:54:58 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     url='https://github.com/dlink/vlib',
     packages=['vlib'],
     name='vlib',
-    version='1.3.0',
+    version='1.3.2',
     scripts=[],
     license='GNU General Public License (GPL)',
     install_requires=map(string.strip, open('requirements.txt').readlines()),
