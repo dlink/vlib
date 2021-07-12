61ab4607088f271c42a55746dc9fb725fd877b76
David Link <david.link@tommicopper.com>
2021-07-12 19:24:04 -0400

    back out last change which broke copy(odict)

--- src/vlib/odict.py
+++ src/vlib/odict.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-class OdictError(Exception): pass
-
 class odict(dict):
     '''odict is an Object Dictionary.  It subclasses builtin dict object.
     It allows dot (.) syntax.
@@ -14,8 +12,8 @@ class odict(dict):
     def __setattr__(self, key, value): self[key] = value
     def __getattr__(self, key):
         if key not in self:
-            raise OdictError("%s does not contain: '%s'"
-                             % (self.__class__.__name__, key))
+            raise AttributeError("%s does not contain: '%s'"
+                                 % (self.__class__.__name__, key))
         return self[key]
 
     def __delattr__(self, key): del self[key]    
