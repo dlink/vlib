72f5350fba91dc6039c06090c26931f484624c65
David Link <dvlink@gmail.com>
2024-02-17 08:36:14 -0500

    Tests: Odict: Fixed Error test

--- tests/lib/test_odict.py
+++ tests/lib/test_odict.py
@@ -1,6 +1,6 @@
 import unittest
 
-from vlib.odict import odict, OdictError
+from vlib.odict import odict
 
 # fixtures
 COLOR = 'red'
@@ -9,5 +9,5 @@ DATA = odict(color=COLOR)
 class TestOdict(unittest.TestCase):
     def test_odict(self):
         self.assertEqual(DATA.color, COLOR)
-        with self.assertRaises(OdictError):
+        with self.assertRaises(AttributeError):
             DATA.non_existent_field
